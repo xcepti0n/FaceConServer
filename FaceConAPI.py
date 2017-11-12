20709f0be2facee1bbd2576c6540fbf3fca40714
xcepti0n <vaibhav.bhatia96@gmail.com>
2017-11-12 12:00:29 +0530

    working API with url detection

--- FaceConAPI.py
+++ FaceConAPI.py
@@ -7,42 +7,60 @@ import numpy as np
 from flask import Flask,request
 from flask_restful import Resource, Api, reqparse
 from sklearn.naive_bayes import GaussianNB
-import pandas as pd
 import os
 import FaceConML
+import urlmatch
 app = Flask(__name__)
 api = Api(app)
 FILE_PATH="./min_user_data.csv"
 @app.route('/')
 class FakeProfile (Resource):
        def post(self):
-              statuses_count= 45
-              followers_count= 23
-              friends_count = 34
-              favourites_count = 333
+              tweets = 0
+              followers = 0
+              following = 0
+              likes = 0
+              lists = 0
+              joined_date = 1390466506
+              bio = 0
+              location = 0
+              has_url = 0
+              #default_values=['0','0','0','0','0','1390466506','','0']
               parser = reqparse.RequestParser()
-              headers_used = ["statuses_count","followers_count","friends_count","favourites_count"]
+              headers_used = ["tweets","followers","following","likes","lists","joined_date","bio","location"]
               for header in headers_used:
+                     print(header)
                      parser.add_argument(header)
               args = parser.parse_args()
-              if(args['statuses_count']):
-                     statuses_count=args['statuses_count']
-              if(args['followers_count']):
-                     followers_count=args['followers_count']
-              if(args['friends_count']):
-                     friends_count = args['friends_count']
-              if(args['favourites_count']):
-                     favourites_count = args['favourites_count']
-              # Headers 
-              headers = ["id","name","screen_name","statuses_count","followers_count","friends_count","favourites_count","profile_type"]
+              for header in headers_used:
+                     print(args[header])
+              if(args['tweets']):
+                     tweets=args['tweets']
+              if(args['followers']):
+                     followers=args['followers']
+              if(args['following']):
+                     following=args['following']
+              if(args['likes']):
+                     likes = args['likes']
+              if(args['joined_date']):
+                     joined_date = args['joined_date']
+              if(args['lists']):
+                     lists = args['lists']
+              if(args['bio']):
+                     bio = args['bio']
+              if(args['location']):
+                     location = 1
+              has_url = urlmatch.has_url(bio)
+              print("tweets: ",tweets,"followers: ",followers,"following: ",following,"likes: ",likes,"joined_date: ",joined_date,"lists: ",lists,"bio: ",bio, "location: ",location,"has_url",has_url)
               #loadcsv file
-              predicted = FaceConML.naive_bayes(int(statuses_count),int(followers_count),int(friends_count),int(favourites_count))
+              #predicted = FaceConML.naive_bayes(int(tweets),int(followers),int(following),int(likes),int(lists),int(joined_date),int(has_url),int(location))
+              predicted, predicted_proba= FaceConML.random_forest_classifier(int(tweets),int(followers),int(following),int(likes),int(lists),int(joined_date),int(has_url),int(location))
               print (predicted)
               if(predicted==1):
                      value="fake profile"
               else:
                      value="legitimate profile"
-              return { 'title': 'FaceCon','value': value}
+              return { 'title': 'FaceCon','profile_type': value,'Probability':predicted_proba}
 api.add_resource(FakeProfile,'/fakeprof')
 if __name__ == '__main__':
        port = int(os.environ.get('PORT', 5000))
--- FaceConML.py
+++ FaceConML.py
@@ -5,20 +5,44 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
-FILE_PATH="./min_user_data.csv"
+FILE_PATH="./user_data_unshuffled.csv"
 # Headers 
 headers = ["id","name","screen_name","statuses_count","followers_count","friends_count","favourites_count","profile_type"]
 #loadcsv file
-def naive_bayes(statuses_count,followers_count,friends_count,favourites_count):
-    model = GaussianNB()
+def predict_using_classifier(model,tweets,followers,following,likes,lists,joined_date,has_url,location):
     dataset = pd.read_csv(FILE_PATH)
     # Get basic statistics of the loaded dataset
     #print(dataset.dtypes)
     #print(dataset.dtypes)
-    X=dataset.iloc[:,3:7]
-    y=dataset.iloc[:,7]
+    X=dataset.iloc[:,0:8]
+    y=dataset.iloc[:,8]
     print(X.shape,y.shape)
+    print(X,y)
     model.fit(X, y)
-    predicted= model.predict([[statuses_count,followers_count,friends_count,favourites_count]])
+    #model.predict_proba([[tweets,followers,following,likes,lists,joined_date,has_url,location]])
+    predicted= model.predict([[tweets,followers,following,likes,lists,joined_date,has_url,location]])
     return predicted
+def naive_bayes(tweets,followers,following,likes,lists,joined_date,has_url,location):
+    model = GaussianNB()
+    return predict_using_classifier(model,tweets,followers,following,likes,lists,joined_date,has_url,location)
 
+def random_forest_classifier(tweets,followers,following,likes,lists,joined_date,has_url,location):
+    model = RandomForestClassifier()
+    dataset = pd.read_csv(FILE_PATH)
+    # Get basic statistics of the loaded dataset
+    #print(dataset.dtypes)
+    #print(dataset.dtypes)
+    X=dataset.iloc[:,0:8]
+    y=dataset.iloc[:,8]
+    #print(X.shape,y.shape)
+    #print(X,y)
+    model.fit(X, y)
+    predicted_proba = model.predict_proba([[tweets,followers,following,likes,lists,joined_date,has_url,location]])
+    predicted= model.predict([[tweets,followers,following,likes,lists,joined_date,has_url,location]])
+    print(predicted,predicted_proba[0][0], predicted_proba[0][1])
+    if(predicted == 0):
+        predicted_proba = predicted_proba[0][0]
+    else:
+        predicted_proba = predicted_proba[0][1]
+    return predicted, predicted_proba
+    #return predict_using_classifier(model,tweets,followers,following,likes,lists,joined_date,has_url,location)
